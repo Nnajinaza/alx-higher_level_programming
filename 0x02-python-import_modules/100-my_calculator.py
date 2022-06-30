a7c8e34981d337bf1e79419d890b35ad82726686
Nnajinaza <nnajinaza@gmail.com>
2022-06-30 23:49:54 +0000

    Task 101

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if ((len(sys.argv) - 1) != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    option = {"+":add, "-":sub, "*":mul, "/":div}
+    option = {"+": add, "-": sub, "*": mul, "/": div}
 
     if sys.argv[2] not in option:
         print("Unknown operator. Available operators: +, -, * and /")
