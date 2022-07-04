d09f133a672921294c3f636630c2fb7698dd1022
Nnajinaza <nnajinaza@gmail.com>
2022-07-04 17:48:46 +0000

    Task 8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,5 +10,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 0:
         tuple_b = (0, 0)
 
-    sum_tuple = (int(tuple_a[0]) + int(tuple_b[0]), int(tuple_a[1]) + int(tuple_b[1]))
+    sum_tuple = (int(tuple_a[0]) + int(tuple_b[0]),
+                 int(tuple_a[1]) + int(tuple_b[1]))
     return sum_tuple
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        return None
+    if sentence is not None:
+        length = len(sentence)
+        if sentence[0] >= 'A' and sentence[0] <= 'Z':
+            return length, sentence[0]
+        elif sentence[0] >= 'a' and sentence[0] <= 'z':
+            return length, sentence[0]
