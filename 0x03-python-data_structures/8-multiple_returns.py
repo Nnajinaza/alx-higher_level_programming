dfd7586c315eb5636d30d0209640e7f358c849cc
Nnajinaza <nnajinaza@gmail.com>
2022-07-04 17:56:06 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,5 @@
 def multiple_returns(sentence):
     if sentence is None:
         return len(sentence), None
-    if sentence is not None:
-        length = len(sentence)
-        return length, sentence[0]
+    elif sentence is not None:
+        return len(sentence), sentence[0]
