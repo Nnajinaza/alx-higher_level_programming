d662c76f0a333ed790553d3478909ee71494eb3d
Nnajinaza <nnajinaza@gmail.com>
2022-07-04 11:01:41 +0000

    Task 5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    for idx in range(len(mylist)):
+        if idx < 0 or idx > len(my_list):
+            return my_list
+        else:
+            mylist[idx] = my_list
+            mylist[idx] = element
+            return mylist
+       ## for idx in range(len(my_list)):
+         ##   my_list[idx] = element
+        ## return my_list
