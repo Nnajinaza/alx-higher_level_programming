b5ebc1b6212c71807517b654321a4a5df8c9bed8
Nnajinaza <nnajinaza@gmail.com>
2022-07-04 17:53:18 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,4 @@ def multiple_returns(sentence):
         return len(sentence), None
     if sentence is not None:
         length = len(sentence)
-        if sentence[0] >= 'A' and sentence[0] <= 'Z':
-            return length, sentence[0]
-        elif sentence[0] >= 'a' and sentence[0] <= 'z':
-            return length, sentence[0]
+        return length, sentence[0]
