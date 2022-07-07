39496e933cd43cbd29925eea17451350930ab140
Nnajinaza <nnajinaza@gmail.com>
2022-07-07 01:08:49 +0000

    Task 11 hot fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    squ_matrix = matrix.copy()
-    return([list(map(lambda x: x ** 2, i)) for i in matrix])
+    return list(map((lambda x: list(map((lambda y: y * y), x))), matrix))
