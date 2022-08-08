2953315d7f6afdcd48caa0104c433cfd0603b8ea
Nnajinaza <nnajinaza@gmail.com>
2022-08-08 12:16:10 +0000

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to return JSON str repr"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return []
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
