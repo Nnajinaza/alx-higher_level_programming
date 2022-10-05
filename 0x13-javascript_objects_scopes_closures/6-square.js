bffe4bdbcd261c1ad302131f1c4a41e4d52b4637
Nnajinaza <nnajinaza@gmail.com>
2022-10-05 00:11:41 +0000

    documentation fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-//Write a class Square that defines a square and inherits from Square of 5-square.js:
+// Write a class Square that defines a square and inherits from Square of 5-square.js:
 //
-//You must use the class notation for defining your class and extends
-//Create an instance method called charPrint(c) that prints the rectangle using the character c
-//If c is undefined, use the character X
+// You must use the class notation for defining your class and extends
+// Create an instance method called charPrint(c) that prints the rectangle using the character c
+// If c is undefined, use the character X
 
 module.exports = class Square extends require('./5-square') {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
-    }
-    else {
+    } else {
       for (let i = 0; i < this.width; i++) {
         for (let j = 0; j < this.height; j++) {
           process.stdout.write(c);
-	}
+        }
         console.log();
       }
     }
