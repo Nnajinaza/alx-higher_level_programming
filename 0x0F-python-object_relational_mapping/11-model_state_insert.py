3bf7fc0d95d52488ddb975aa69960ac3b477f21b
Nnajinaza <nnajinaza@gmail.com>
2022-10-10 01:51:02 +0000

    task 10

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+    Write a script that adds the State object “Louisiana” to
+    the database hbtn_0e_6_usa
+"""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, Session
