f1743e273959170f4a78c90262968314a19f1ab8
Nnajinaza <nnajinaza@gmail.com>
2022-10-10 02:20:52 +0000

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,8 +12,19 @@ Base = declarative_base()
 
 
 class City(Base):
+    """
+    Your script should take 3 arguments: mysql username,
+    mysql password and database name
+    You must use the module SQLAlchemy
+    You must import State and Base from model_state - from
+    model_state import Base, State
+    Your script should connect to a MySQL server running
+    on localhost at port 3306
+    Results must be sorted in ascending order by cities.id
+    Results must be display as they are in the example
+    below (<state name>: (<city id>) <city name>)
+    """
     __tablename__ = 'cities'
-
     id = Column(Integer(), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"))
